77109c1891a1c175a9db27567dce8c90c3657aed
elliec28 <elinor.campion@gmail.com>
2025-05-25 00:10:24 -0700

    test getting api data

--- app.jsx
+++ app.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import useSWR from 'swr'; // Import useSWR from swr package
 
 const root = ReactDOM.createRoot(document.body);
+// Created function to handle API request
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const Swr = () => {
+  const {
+    data: name,
+    url
+  } = useSWR('https://pokeapi.co/v2/1', fetcher);
+}
+
+  // Handles error and loading state
+  //if (error) return <div className='failed'>failed to load</div>;
+  //if (isValidating) return <div className="Loading">Loading...</div>;
+
 root.render(<App />);
 
 function App() {
   return (
-    <div className="banana">
-      <h1>React me! Test!</h1>
-    </div>
+      
+      <div className="banana">
+        <h1 style={{color: "white"}}>Pokedex</h1>
+      </div>
+
   )
 }
